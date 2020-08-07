9dbb8a2b3e1f657977f9bb70c04ac8557b135552
AlexKutsar <alekcandr_123qw@mail.ru>
2020-08-07 15:04:49 +0300

    add placing building on a grid

---
+++ Assets/Scripts/Buildind.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buildind : MonoBehaviour
+{
+    [SerializeField] private Vector2Int _size = Vector2Int.one;
+    [SerializeField] private Color _colorGizmos = Color.green;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        for(int x = 0; x < _size.x; x++)
+        {
+            for (int y = 0; y < _size.y; y++)
+            {
+                Gizmos.color = _colorGizmos;
+                Gizmos.DrawCube(transform.position + new Vector3(x, 0, y), new Vector3(1, 0.1f, 1));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BuildingsGrid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingsGrid : MonoBehaviour
+{
+    [SerializeField] private Vector2Int _sizeGrid = new Vector2Int(10, 10);
+
+    private Buildind[,] _grid;
+    private Buildind _flyingBuilding = null;
+    private Camera _camera;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _grid = new Buildind[_sizeGrid.x, _sizeGrid.y];
+        _camera = Camera.main;
+    }
+
+    public void StartPlacingBuilding(Buildind buildindPrefab)
+    {
+        if (_flyingBuilding != null)
+        {
+            Destroy(_flyingBuilding);
+        }
+
+        _flyingBuilding = Instantiate(buildindPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_flyingBuilding != null)
+        {
+            var groundPlace = new Plane(Vector3.up, Vector3.zero);
+            Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+            
+            if(groundPlace.Raycast(ray , out float position))
+            {
+                Vector3 worldPosition = ray.GetPoint(position);
+
+                int x = Mathf.RoundToInt(worldPosition.x);
+                int y = Mathf.RoundToInt(worldPosition.z);
+
+                _flyingBuilding.transform.position = new Vector3(x, 0, y);
+
+                if(Input.GetMouseButtonDown(0))
+                {
+                    _flyingBuilding = null;
+                }
+            }
+        }
+    }
+}
