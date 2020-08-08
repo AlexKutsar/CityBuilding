55bf9c3eaa8831ef54658e9735bfc9292e4f6a1a
AlexKutsar <alekcandr_123qw@mail.ru>
2020-08-08 11:55:44 +0300

    add test move and rotate camera

--- Assets/Scripts/CameraMoveRotate.cs
+++ Assets/Scripts/CameraMoveRotate.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraMoveRotate : MonoBehaviour
 {
     [SerializeField] private float _speedRotation = 3f;
+    [SerializeField] private float _speedMove = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +15,39 @@ public class CameraMoveRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        if (Input.mousePosition.x >= (Screen.width - 2.25f))
+        {
+            transform.position += new Vector3(_speedMove * Time.deltaTime, 0.0f, 0.0f);
+        }
+        else if (Input.mousePosition.x <= 2.25f)
+        {
+            transform.position -= new Vector3(_speedMove * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+        if (Input.mousePosition.y >= (Screen.height - 2.25f))
+        {
+            transform.position += new Vector3(0.0f, 0.0f, _speedMove * Time.deltaTime);
+        }
+        else if (Input.mousePosition.y <= 2.25f)
+        {
+            transform.position -= new Vector3(0.0f, 0.0f, _speedMove * Time.deltaTime);
+        }
+    }
+
+    private void Rotate()
+    {
         if (Input.GetMouseButton(1))
         {
             float mouseX = Input.GetAxis("Mouse X");
             float mouseY = Input.GetAxis("Mouse Y");
             //Quaternion.AngleAxis(mouseX* 100, Vector3.up);
-            transform.Rotate(mouseY * _speedRotation * Time.deltaTime, mouseX * _speedRotation * Time.deltaTime, 0);
+            transform.Rotate(-mouseY * _speedRotation * Time.deltaTime, -mouseX * _speedRotation * Time.deltaTime, 0);
             Debug.Log(mouseX * _speedRotation * Time.deltaTime);
         }
     }
