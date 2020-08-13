2b444c016b2b36ed8b609501788f88a0637b520a
AlexKutsar <alekcandr_123qw@mail.ru>
2020-08-13 16:23:51 +0300

    add check road in prefab buildings

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Building : MonoBehaviour
 {
     public bool IsResourceBuilding = false;
+    public Transform checkRoad = null;
     public List<BuildingLevelData> LevelsData = new List<BuildingLevelData>();
     [SerializeField] private int _currentLevel = 0;
     public int CurrentLevel => _currentLevel;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
     }
 
     // Update is called once per frame
